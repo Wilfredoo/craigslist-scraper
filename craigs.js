cae6faf96635c60b5c4a0751dcd73a90cdde0c55
Wilfredo <casas.farach@gmail.com>
2020-04-09 22:46:08 +0200

    got one email

--- craigs.js
+++ craigs.js
@@ -1,14 +1,24 @@
 const puppeteer = require("puppeteer");
+const fs = require("fs-extra");
 
 (async () => {
   const browser = await puppeteer.launch({ headless: false, slowMo: 1000 });
   const page = await browser.newPage();
+  await page.setDefaultNavigationTimeout(0);
+
   await page.goto(
     "https://newyork.craigslist.org/brk/trd/d/brooklyn-motorcycle-dealership-looking/7103404321.html"
   );
-  await page.click("  button");
 
-  await page.waitForSelector("//aside");
+  await page.click("button");
+
+  await page.waitForSelector("aside");
+
+  const element = await page.$(".reply-email-address");
+  console.log("element", element);
+
+  const email = await page.evaluate((element) => element.textContent, element);
+  await fs.appendFile("out.csv", `"${email}"\n`);
 
   // await page.goto("https://newyork.craigslist.org/search/trd?");
 
@@ -27,3 +37,12 @@ const puppeteer = require("puppeteer");
 
   await browser.close();
 })();
+
+// for (const li of lis) {
+//   const name = await li.$eval("h2", (h2) => h2.innerText);
+//   console.log(name);
+// }
+
+// const fs = require('fs-extra')
+// await fs.writeFile('out.csv', 'section, name\n')
+// await fs.appendFile('out.csv', `"${buttonName}", "${name}"\n`)

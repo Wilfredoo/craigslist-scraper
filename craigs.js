ea9a1eb80ca14b00c37b0d56fd04289e0d474c3e
Wilfredo <casas.farach@gmail.com>
2020-04-10 10:21:28 +0200

    running through each category

--- craigs.js
+++ craigs.js
@@ -1,17 +1,17 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs-extra");
 const scrapeCategory = async (category) => {
-  const browser = await puppeteer.launch({ headless: false, slowMo: 1000 });
+  const browser = await puppeteer.launch({ headless: true, slowMo: 1000 });
   const page = await browser.newPage();
   await page.setDefaultNavigationTimeout(0);
   await page.goto(`https://newyork.craigslist.org/search/${category}?`);
   let linkHandlers = await page.$x("(//a[@class='result-title hdrlnk'])");
   let i;
   for (i = 0; i < 120; i++) {
+    console.log("loop number: ", i, " category: ", category);
     await linkHandlers[i].click();
     await page.waitForNavigation();
     const button = await page.$("button");
-    console.log("gimme that button", button);
     if (button !== null) {
       await page.click("button");
       await page.waitForSelector("aside");
@@ -32,10 +32,7 @@ const scrapeCategory = async (category) => {
   const nycA = "fbh";
   const nycB = "lab";
   const nycC = "trd";
-  const nycX = "tch";
-
-  // scrapeCategory(nycA);
-  // scrapeCategory(nycB);
-  // scrapeCategory(nycC);
-  scrapeCategory(nycX);
+  await scrapeCategory(nycA);
+  await scrapeCategory(nycB);
+  await scrapeCategory(nycC);
 })();
